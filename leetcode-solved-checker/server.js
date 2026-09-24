06286c1c2c636b08acb313b2b13699988f8c8aff
aravinth0237-ship-it <aravinth0237@gmail.com>
2026-09-24 20:29:01 +0530

    Update server.js

--- leetcode-solved-checker/server.js
+++ leetcode-solved-checker/server.js
@@ -89,7 +89,7 @@ app.get("/api/profile", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
